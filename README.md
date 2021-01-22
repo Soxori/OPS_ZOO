9a5ab56e1327c5609d0204c459cea793b93c5a8a
Jakub Panek <jakub.panek@educanet.cz>
2021-01-22 12:47:31 +0100

    mam chut do zivota delkem

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 "# OPS_ZOO" 
 "# OPS_ZOO" 
 "# OPSZOO2" 
+"# OPSZOO2" 
---
+++ src/main/java/cz/educanet/webik/AnimalResource.java
@@ -0,0 +1,58 @@
+package cz.educanet.webik;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Path("animals")
+public class AnimalResource {
+    @Inject
+    private AnimalManager manager;
+
+    public static List<Animal> animals = new ArrayList<Animal>();
+
+    @GET
+    public Response getAll() {
+        return Response.ok(animals).build();
+    }
+
+    @POST
+    @Path("create")
+    public Response createAnimal(
+            @FormParam("id") int id,
+            @FormParam("Name") String name,
+            @FormParam("Age") int age,
+            @FormParam("Weight") int weigth,
+            @FormParam("Gender") String gender
+    ) {
+        Animal animal = new Animal(id, name, age, weigth, gender);
+        if (animalCheck(animal)) {
+            return Response.ok("This animal already exists").build();
+        } else {
+            animals.add(animal);
+            return Response.ok("Animal:" + animal + "is added").build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response removeAnimal(@PathParam("id") int id) {
+        manager.animal = null;
+        return Response.ok("Done").build();
+    }
+
+    public Boolean animalCheck(Animal animal) {
+        for (int i = 0; i < animals.size(); i++) {
+            if (animals.get(i).getName().equals(animal.getName())) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+}
